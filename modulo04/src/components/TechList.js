9fa735d2c33536c892e84cd032b3b3fe41cace95
Neder <nedersimoes@gmail.com>
2020-12-01 16:02:27 -0400

    conceitos | estado, imutabilidade, props

--- modulo04/src/components/TechList.js
+++ modulo04/src/components/TechList.js
@@ -1,22 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import TechItem from "./TechItem";
 
 class TechList extends Component {
+  state = {
+    newTech: "",
+    techs: [],
+  };
 
-    state = {
-        techs: [
-            'Node.js',
-            'ReactJS',
-            'React Native'
-        ]
-    };
+  componentDidMount() {
+    const techs = localStorage.getItem("techs");
 
-    render(){
-        return(
-            <ul>
-                { this.state.techs.map(tech => <li> key={tech}{tech}</li>)}
-            </ul>
-        );
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
     }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem("techs", JSON.stringify(this.state.techs));
+    }
+  }
+
+  componentWillUnmount() {}
+
+  handleInputChange = (e) => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: "",
+    });
+  };
+
+  handleDelete = (tech) => {
+    this.setState({
+      techs: this.state.techs.filter((t) => t !== tech),
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map((tech) => (
+            <TechItem
+              key={tech}
+              tech={tech}
+              onDelete={() => this.handleDelete(tech)}
+            />
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  }
 }
 
 export default TechList;
