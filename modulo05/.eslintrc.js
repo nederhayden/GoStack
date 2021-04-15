2471e9f0d7a477e158b3c2eda3c31bed9fecd061
Neder <nedersimoes@gmail.com>
2021-04-15 16:00:07 -0400

    continuacao do modulo05

---
+++ modulo05/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2021,
+        sourceType: 'module',
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        'react/jsx-filename-extension': [
+            'warn',
+            { extensions: ['.jsx', '.js'] },
+        ],
+        'import/prefer-default-export': 'off',
+    },
+};
