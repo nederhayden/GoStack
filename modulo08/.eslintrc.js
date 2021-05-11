a84a31109d5a54b7c960080e43bd4b9cb102f9fc
Neder <nedersimoes@gmail.com>
2021-05-11 12:46:16 -0400

    inserindo plugin no eslint

--- modulo08/.eslintrc.js
+++ modulo08/.eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
         ecmaVersion: 12,
         sourceType: 'module',
     },
-    plugins: ['react', 'prettier'],
+    plugins: ['react', 'prettier', 'react-hooks'],
     rules: {
         'prettier/prettier': [
             'error',
